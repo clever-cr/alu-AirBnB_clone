5f3857d683a43f8617c997885520a98e86b03753
arisene <karuretwaarsene744@gmail.com>
2023-05-22 22:06:01 +0200

    base-model

---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+     """Base class for other classes with common attributes/methods."""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new instance of the BaseModel class
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def save(self):
+        """Update the 'updated_at' attribute with the current datetime."""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+         """
+        Return a dictionary representation of the instance.
+
+        Returns:
+            dict: A dictionary containing all the attributes of the instance.
+                  Includes the '__class__' key with the class name,
+                  'created_at' and 'updated_at' as ISO-formatted strings.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+         """Return a string representation of the instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
