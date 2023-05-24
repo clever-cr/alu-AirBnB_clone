300903911759f4c9835c4a7c199a521e4df6bfdc
clever-cr <cleverumuhuza@gmail.com>
2023-05-24 12:20:01 +0200

    remove unnecessary print

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from models import storage
 from models.base_model import BaseModel
-print("hhhhhhhhhh")
+
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
