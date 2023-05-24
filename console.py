dafac14caefb49297edb9e7bdf4f51048cb19c5c
clever-cr <cleverumuhuza@gmail.com>
2023-05-24 15:05:36 +0200

    fix issue

--- console.py
+++ console.py
@@ -106,6 +106,10 @@ class HBNBCommand(cmd.Cmd):
                 setattr(item, arr[2], arr[3])
                 storage.save()
 
+    def help_update(self):
+        print("update command to \
+        update the a single object property")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
